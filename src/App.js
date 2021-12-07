481d0d1f7860afd13e65c74e7f89fa1ff147b7f6
Darbaz Ali <darbaz.me@gmail.com>
2021-12-07 15:05:08 +0300

    animate icon

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-
+import { useState } from "react"
+import { Player, Controls } from "@lottiefiles/react-lottie-player"
+import santaPopup from "./santa-pop-up.json"
 
 const App = () => {
-  return (
-    <h1>
-      Lottie Animations
-    </h1>
-  )
+    const [showAnimation, setShowAnimation] = useState(false)
+
+    const handleClick = () => setShowAnimation(true)
+    return (
+        <div>
+            {showAnimation && (
+                <Player
+                    keepLastFrame
+                    autoplay
+                    loop
+                    speed={1}
+                    src={santaPopup}
+                    style={{ height: "300px", width: "300px" }}>
+                    <Controls
+                        visible={true}
+                        buttons={["play", "repeat", "frame", "debug"]}
+                    />
+                </Player>
+            )}
+
+            <button onClick={handleClick}>Animate</button>
+        </div>
+    )
 }
 
 export default App
